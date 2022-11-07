f4bc441c7a8312a88729d46e8b44e28fdc830193
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-07 12:33:08 -0500

    stupid coach is being stupid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    if params[:question]
+      if params[:question].downcase == "i am going to work"
+        @answer = "Great!"
+      elsif params[:question].end_with?('?')
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
 end
